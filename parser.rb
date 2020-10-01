ea3e2d4e522baafe76b455e27240edd2d83ef03d
nij4life <TjOdapQzPaswen88>
2020-10-01 14:42:13 +0300

    doc: update examples block

--- parser.rb
+++ parser.rb
@@ -12,14 +12,14 @@ Choice.options do
     short '-u'
     long '--url=URL'
     desc 'This is the address of the page we need'
-    default 'https://av.by/'
+    validate /^https:\/\/cars\.av\.by\//
   end
 
   option :url_type do
     short '-t'
     long '--url_type=URL_TYPE'
     desc 'This is the information type from page which we need. (categories or products)'
-    default 'categories'
+    validate /^(categories||products)$/
   end
 
   option :recursive do
@@ -33,29 +33,23 @@ Choice.options do
     short '-s'
     long '--skip_products=SKIP_PRODUCTS'
     desc 'Skip collecting product data. (true or false)'
-    default 'true'
+    default 'false'
   end
-
-  separator ''
-  separator 'Common options: '
-  separator '-u or --url   # Url адрес нужной нам категории'
-  separator '-t or --url_type   # Тип переданного url адреса (category or product)'
-  separator '-r or --recursive   # Опция рекурсивного обхода дерева категорий (true or false)'
-  separator '-s or --skip_products   # Опция для указания, пропустить ли скачивание продуктов (true or false)'
-  separator 'По умолчанию при запуске:  # -u https://cars.av.by/ -t category -r false -s false'
-  separator 'Examples: '
-  separator 'ruby parser.rb -r true   # Собрать все дерево категорий'
-  separator 'ruby parser.rb -r true -s false   # Собрать все дерево категорий и сохранить продукты'
-  separator 'ruby parser.rb -u https://cars.av.by/acura   # Обновить категорию acura'
-  separator 'ruby parser.rb -u https://cars.av.by/acura -r true   # Собрать дерево подкатегорий для acura'
-  separator 'ruby parser.rb -u https://cars.av.by/acura -r true -s false   # Собрать дерево подкатегорий для acura и сохранить продукты'
-  separator 'ruby parser.rb -u https://cars.av.by/acura/mdx -s false   # Обновить категорию acura -> mdx и скачать с нее продукты'
 
   option :help do
     short '-h'
     long '--help'
     desc 'Show this message'
   end
+
+  separator ''
+  separator 'Examples: '
+  separator 'ruby parser.rb -u https://cars.av.by -t categories -r true -s true          # Собрать все дерево категорий'
+  separator 'ruby parser.rb -u https://cars.av.by -t categories -r true                  # Собрать все дерево категорий и сохранить продукты'
+  separator 'ruby parser.rb -u https://cars.av.by/acura -t categories                    # Собрать категорию "acura" и сохранить продукты'
+  separator 'ruby parser.rb -u https://cars.av.by/acura -t categories -r true -s true    # Собрать дерево подкатегорий для "acura"'
+  separator 'ruby parser.rb -u https://cars.av.by/acura -t categories -r true            # Собрать дерево подкатегорий для "acura" и сохранить продукты'
+  separator 'ruby parser.rb -u https://cars.av.by/acura/mdx -t categories                # Собрать категорию "acura -> mdx" и сохранить продукты'
 end
 
 puts Choice.choices[:help]
@@ -63,3 +57,5 @@ puts Choice.choices[:help]
 av = AV::AV_parser.new(Choice.choices)
 
 av.search
+
+# Добавить отловить ошибку если url кривой, или ничего не найдено и т д. Это будет после запроса к nokogiry
